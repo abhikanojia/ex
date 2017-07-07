71f73e90459e4300b501d61d89bf64e83acc12d9
abhishek.kanojia <abhishek.kanojia@vinsol.com>
2017-07-07 10:40:58 +0530

    Created occurence hash program using object oriented manner

--- occurence_hash.rb
+++ occurence_hash.rb
@@ -1,22 +1,31 @@
-# Public: Counts the occurances of characters in given string.
-#
-# string  - The String to be used for counting characters
-# which is passed as command line argument.
+# Public: Method for counting number of characters in given string.
+# All methods are module methods and should be called on the OccurenceHash
+# class object.
 #
 # Examples
 #
-#  ruby occurence_hash.rb string
-#  # => {"s"=>1, "t"=>1, "r"=>1, "i"=>1, "n"=>1, "g"=>1}
-#
-# Returns the hash having characters as their keys
-# and their no. of occurences as values
-if ARGV.empty?
-  print 'Please provide an input'
-  exit
-else
-  # considering multiple arguments as a single string
-  string = ARGV.join(' ')
-  histo = Hash.new(0)
-  string.chars.each { |x| histo[x] += 1 if x.between?('A', 'z') }
-  print histo
+#   obj = OccurenceHash.new(string)
+#   obj.get_occurence_hash()
+#   # => {"s"=>1, "t"=>1, "r"=>1, "i"=>1, "n"=>1, "g"=>1}
+class OccurenceHash
+  attr_reader :string
+
+  def initialize(string)
+    if string.empty?
+      print 'Please provide an input'
+      exit
+    else
+      # considering multiple arguments as a single string
+      @string = string.join(' ')
+    end
+  end
+
+  def get_occurence_hash
+    histo = Hash.new(0)
+    @string.chars.each { |x| histo[x] += 1 if x.between?('A', 'z') }
+    print histo
+  end
 end
+
+obj = OccurenceHash.new(ARGV)
+obj.get_occurence_hash()
