910f8910e6e8c65890df638bf54384fe991e9257
Abhishek kanojia <abhishekka3193@gmail.com>
2017-07-09 23:50:29 +0530

    Vehicles exercise complete and submitted on bin

--- vehicle.rb
+++ vehicle.rb
@@ -1,18 +1,29 @@
+# Public: Class for printing Vehicles detail.
+#
+# bike_name  - The Integer upto which series to be printed passed as
+# command line argument.
+#
+# bike_price  - Price of bike, command line argument.
+#
+# bike_dealer - Bike dealer, command line argument.
+#
+# percent_price_increase - Increase in percent price of bike,
+# command line argument.
 class Vehicle
-  attr_reader   :bike_name, :bike_price
+  attr_reader :bike_name, :bike_price
 
   def initialize(bike_name, bike_price)
     @bike_name   = bike_name.to_s
-    @bike_price  = Float(bike_price)
+    @bike_price  = bike_price
   end
 
   def to_s
-    "Bike Name: #{bike_name} \nBike Price: #{bike_price} \n"
+    "Bike Name: #{bike_name}\nBike Price: #{bike_price}\n"
   end
 end
-
+# Bike class inherits Vehicle class
 class Bike < Vehicle
-  attr_reader :bike_dealer, :percent_price_increase, :bike_price
+  attr_reader :bike_dealer, :percent_price_increase
 
   def initialize(bike_name, bike_price, bike_dealer, percent_price_increase)
     super(bike_name, bike_price)
@@ -21,20 +32,22 @@ class Bike < Vehicle
   end
 
   def to_s
-    super + "Bike Dealer: #{bike_dealer}\n"
+    super + "Bike Dealer: #{bike_dealer}"
   end
 
   def price_after_increase
-    puts '1'
-    bike_price += bike_price * (percent_price_increase / 100)
-    puts 'b'
+    increase_in_price = Float(@bike_price) * (@percent_price_increase / 100)
+    @bike_price = Float(@bike_price) + increase_in_price
+    self
   end
 end
 
-if ARGV.empty?
-  print "Please provide an input"
+if ARGV[0].nil? || ARGV[1].nil? || ARGV[2].nil? || ARGV[3].nil?
+  print 'Please provide an input'
   exit
 else
   bike = Bike.new(ARGV[0], ARGV[1], ARGV[2], ARGV[3])
-  bike.price_after_increase
+  puts bike
+  puts "\nAfter #{ARGV[3].to_f} percent hike in price:"
+  puts bike.price_after_increase
 end
