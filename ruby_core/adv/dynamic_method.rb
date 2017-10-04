058bac73c04a75be947cbd582e7e8042c63c3ad2
abhishekkanojia <abhishek.kanojia@vinsol.com>
2017-10-04 17:26:08 +0530

    Created new programs

--- ruby_core/adv/dynamic_method.rb
+++ ruby_core/adv/dynamic_method.rb
@@ -1,12 +1,33 @@
-# dynamic class
-# change class name
-class DynamicMethod < String
+# param list method return methods
+class Array
+  def params_list
+    flatten.delete_if { |item| item =~ /req|opt/ }
+  end
+
+  def segregate
+    hash = Hash.new([])
+    each do |arr|
+      hash[arr.first] += [arr[1]]
+    end
+    hash
+  end
+end
+
+# Interactive Class
+class InteractiveClass < String
+
+  VOWELS = /[aeiou]/i
+
   def replace_vowels_with(character)
-    gsub(/[aeiou]/i, character)
+    gsub(VOWELS, character)
   end
 
   def exclude?(arg)
     !(include? arg)
+  end
+
+  def abx(a, b, c=1)
+    print a,b,c
   end
 
   def list_of_methods
@@ -17,29 +38,51 @@ class DynamicMethod < String
     split(' ').reverse.join(' ')
   end
 
-  def call_method(method_name, *arg)
+  def params(method_name)
+    self.method(method_name.to_sym).parameters.segregate
+  end
+
+
+  def execute(method_name, *arg)
     return public_send(method_name, *arg) if list_of_methods.include? method_name.to_sym
     'No Method Exists by that name'
-  rescue ArgumentError => e
-    puts "Please provide argument to call #{method_name} function: "
-    arg = gets.chomp
-    call_method(method_name, arg)
   end
 end
 
-# handle exception outside
-# print number of arguments required by function
-puts 'Please enter the string to call function on: '
-input_string = gets.chomp
-puts
+begin
+  puts 'Please enter the string to call function on: '
+  input_string = gets.chomp
+  puts
 
-string = DynamicMethod.new(input_string)
+  string = InteractiveClass.new(input_string)
 
-puts 'List of methods available for this input.'
-p string.list_of_methods
-puts
+  puts 'List of methods available for this input.'
+  p string.list_of_methods
+  puts
 
-puts 'Enter the name of method you want to execute from list'
-method_to_execute = gets.chomp
+  puts 'Enter the name of method you want to execute from list'
+  method_to_execute = gets.chomp
 
-p string.call_method(method_to_execute)
+  p string.execute(method_to_execute)
+
+rescue ArgumentError => e
+  params = string.params(method_to_execute)
+
+  parameters_required = []
+
+  if params.key? :req
+    params[:req].each do |req_param|
+      print "Please provide mandatory parameter: #{req_param} "
+      parameters_required << gets.chomp
+    end
+  end
+
+  if params.key? :opt
+    params[:opt].each do |req_param|
+      print "Please provide optional parameter: #{req_param} "
+      parameters_required << gets.chomp
+    end
+  end
+  parameters_required.delete_if(&:empty?)
+  p string.execute(method_to_execute, *parameters_required)
+end
