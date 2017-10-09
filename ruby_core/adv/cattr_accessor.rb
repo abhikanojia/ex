50fa5d75ec3f9bdf2168d85314c907f9e513484b
Abhishek kanojia <abhishekka3193@gmail.com>
2017-10-09 23:49:00 +0530

    Cattr completed

--- ruby_core/adv/cattr_accessor.rb
+++ ruby_core/adv/cattr_accessor.rb
@@ -1,16 +1,18 @@
-class Module
-  def cattr_accessor(*attributes)
-    p symbol_methods = Hash.new(0)
-    p attributes.first
-    symbol_methods[attributes.first] = attributes[1].keys
-    exit
-    # if options_has_accessor_with_reader_writer?(options)
-    #   abort 'Error Occured'
-    # end
+class CattrAccessorError < StandardError
+  def message(msg)
+    'Cannot set instance_accessor with instance_reader/writer'
+  end
+end
 
+class Module
+
+  DEFAULT_OPTIONS = { instance_reader: true, instance_writer: true, instance_accessor: true }
+
+  def cattr_accessor(*attributes, options)
+    DEFAULT_OPTIONS.merge!(options)
+    raise CattrAccessorError if !DEFAULT_OPTIONS[:instance_accessor]
     attributes.each do |attr|
       cls_var = "@@#{attr}"
-
       define_singleton_method("#{attr}") do
         class_variable_get cls_var
       end
@@ -18,39 +20,22 @@ class Module
       define_singleton_method("#{attr}=") do |value|
         class_variable_set cls_var, value
       end
-
-      create_instance_reader_method(attr) if options[:instance_reader]
-
-      create_instance_writer_method(attr) if options[:instance_writer]
+      create_instance_reader_method(attr) if DEFAULT_OPTIONS[:instance_reader] || DEFAULT_OPTIONS[:instance_accessor]
+      create_instance_writer_method(attr) if DEFAULT_OPTIONS[:instance_writer] || DEFAULT_OPTIONS[:instance_accessor]
     end
   end
 
   private
-
-  def options_has_accessor_with_reader_writer?(options)
-    if options.keys.include? :instance_accessor
-      # check if keys has instance_reader/writer
-      options.none? { |x| x == :instance_reader || x == :instance_writer }
-    end
-  end
 
   def create_instance_reader_method(attr)
     ins_var = "@@#{attr}"
     define_method("#{attr}") do
       self.class.class_variable_get ins_var
     end
-
-    define_method("#{attr}=") do |value|
-      self.class.class_variable_set ins_var, value
-    end
   end
 
   def create_instance_writer_method(attr)
     ins_var = "@@#{attr}"
-    define_method("#{attr}") do
-      class_variable_get ins_var
-    end
-
     define_method("#{attr}=") do |value|
       class_variable_set ins_var, value
     end
@@ -58,14 +43,25 @@ class Module
 end
 
 class Person
-  cattr_accessor :hair_colors, instance_writer: false, instance_reader: true
-  # cattr_accessor :hair_colors, instance_accessor: false
-  cattr_accessor :colors, instance_accessor: true
+  cattr_accessor :hair_colors, :address, instance_writer: true, instance_reader: false, instance_accessor: true
 end
+
+class Male < Person
+end
+
+
 
 Person.hair_colors = [:brown, :black, :blonde, :red]
 p Person.hair_colors
 p Person.new.hair_colors
 
-# p Person.singleton_methods
+
+p Male.hair_colors << :blonde
+
+p Person.hair_colors
+
+
+Person.address = "patel nagar new delhi"
+p Person.address
+
 # p Person.instance_methods
