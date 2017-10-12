dd02fae693d1f381e2a90c27324d9e57fccbc80f
abhishekkanojia <abhishek.kanojia@vinsol.com>
2017-10-12 12:27:51 +0530

    Changed cattr_accessor

--- ruby_core/adv/cattr_accessor.rb
+++ ruby_core/adv/cattr_accessor.rb
@@ -1,3 +1,9 @@
+# Invalid Argument
+class InvalidArgument < ArgumentError
+  def initialize(msg = 'Invalid Argument provided for cattr_accessor. Argument must be Symbol')
+    super
+  end
+end
 # Cattr error
 class CattrAccessorError < StandardError
   def initialize(msg = 'Cannot set instance_accessor with instance_reader/writer.')
@@ -6,7 +12,9 @@ class CattrAccessorError < StandardError
 end
 # Module CattrAccessor
 module CattrAccessor
+
   def cattr_accessor(*attributes)
+    raise InvalidArgument if !valid_attributes(*attributes)
     options = attributes.select { |element| element.is_a? Hash }.first
 
     raise CattrAccessorError if options_has_reader_writer_with_accessor?(options)
@@ -14,45 +22,51 @@ module CattrAccessor
       next if attribute.is_a? Hash
 
       class_variable = "@@#{attribute}"
-      define_singleton_method("#{attribute}") do
-        class_variable_get class_variable
-      end
 
-      define_singleton_method("#{attribute}=") do |value|
-        class_variable_set class_variable
-      end
+      create_class_reader(attribute)
+      create_class_writer(attribute)
 
-      if options.nil?
-        create_instance_reader_writer(attribute, class_variable)
+      if options_given?(options)
+        create_instance_accessor(attribute)
       else
-        create_instance_accessor(attribute, options, class_variable)
+        create_instance_reader_method(attribute) if instance_reader_needed?(options)
+        create_instance_writer_method(attribute) if instance_writer_needed?(options)
       end
     end
   end
 
   private
 
+  def options_given?(options)
+    options.nil? || options.empty?
+  end
+
+  def valid_attributes(*arguments)
+    arguments.all? { |argument| [Hash, Symbol].include? argument.class }
+  end
+
+  def create_class_reader(argument)
+    define_singleton_method("#{argument}") do
+      class_variable_get class_variable
+    end
+  end
+
+  def create_class_writer(argument)
+    define_singleton_method("#{argument}=") do |value|
+      class_variable_set class_variable
+    end
+  end
+
   def options_has_reader_writer_with_accessor?(options)
-    if !options.nil?
+    if !options_given?(options)
       options.key?(:instance_accessor) && (options.key?(:instance_reader) || options.key?(:instance_writer))
     end
   end
 
-  def create_instance_reader_writer(attribute, class_variable)
-    create_instance_reader_method(attribute, class_variable)
-    create_instance_writer_method(attribute, class_variable)
+  def create_instance_accessor(attribute)
+    create_instance_reader_method(attribute)
+    create_instance_writer_method(attribute)
   end
-
-  def create_instance_accessor(attribute, options, class_variable)
-    if instance_reader_needed?(options)
-      create_instance_reader_method(attribute, class_variable)
-    end
-
-    if instance_writer_needed?(options)
-      create_instance_writer_method(attribute, class_variable)
-    end
-  end
-
 
   def instance_reader_needed?(options)
     options[:instance_accessor] || options[:instance_reader]
@@ -62,13 +76,13 @@ module CattrAccessor
     options[:instance_accessor] || options[:instance_writer]
   end
 
-  def create_instance_reader_method(attr, class_variable)
+  def create_instance_reader_method(attr)
     define_method("#{attr}") do
       self.class.class_variable_get class_variable
     end
   end
 
-  def create_instance_writer_method(attr, class_variable)
+  def create_instance_writer_method(attr)
     define_method("#{attr}=") do |value|
       self.class.class_variable_set class_variable, value
     end
@@ -78,7 +92,7 @@ end
 # person class
 class Person
   extend CattrAccessor
-  cattr_accessor :hair_colors, instance_reader: false, instance_writer: true
+  cattr_accessor :hair_colors, instance_writer: true, instance_reader: false
 end
 # male class
 class Male < Person
